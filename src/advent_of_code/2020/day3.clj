e4ec45e4b48d4cde4bd934e1ee4a13c0a5b60908
suereey <suereey@gmail.com>
2023-04-26 12:13:34 -0700

    update

--- src/advent_of_code/2020/day3.clj
+++ src/advent_of_code/2020/day3.clj
@@ -1,4 +1,5 @@
-(ns advent-of-code.2020.day3)
+(ns advent-of-code.2020.day3
+  (require '[clojure.math.numeric-tower :as math]))
 
 (defn file->seq
   [filename]
@@ -6,22 +7,20 @@
       (slurp)
       (clojure.string/split #"\n")))
 
-;; input-s ["...." "...."]
-;; output-s locations can be checked -> [[0 0] [1 3] [] ...]
-(defn checked-location-s
+;; This is the location (regardless of the grid) that will be checked based on the [right down]
+;; for example, if right = 2 down = 1; it will return a lazy sequence [[0 0] [1 3] [2 6] ...]
+(defn slope-location-s
   [[right down]]
   (map (fn [row]
          [(* row down) (* row right)])
        (iterate inc 0)))
 
+;; The lazy seq was generated in the above function
+;; the filter-location-s function take the seq based on grid's total row
 (defn filter-location-s
-  [[right down] total-row]
-  (take (int (/ total-row down)) (checked-location-s [right down])))
+  [[right down] count-grid-row]
+  (take (Math/ceil (/ count-grid-row down)) (slope-location-s [right down])))
 
-(defn tree?
-  [char]
-  (or (= char \X)
-      (= char \#)))
 
 (defn location-char
   [grid location]
@@ -31,7 +30,12 @@
         location-char    (nth repeated-pattern col)]
     location-char))
 
-(defn num-tree-solution
+(defn tree?
+  [char]
+  (or (= char \X)
+      (= char \#)))
+
+(defn count-tree
   [grid [right down]]
   (let [location-s      (filter-location-s [right down] (count grid))
         location-char-s (mapv #(location-char grid %) location-s)]
@@ -51,15 +55,8 @@
    "..."
    "..."]
 
-  (take 5 (checked-location-s [3 1]))
+  (take 5 (slope-location-s [3 1]))
   #_=> ([0 0] [1 3] [2 6] [3 9] [4 12])
-  ;; question why map works but not map v?
-  ;(defn checked-location-s [right down]
-  ;  (mapv (fn [row]
-  ;         [(* row down) (* row right)])
-  ;       (iterate inc 0)))
-  ;(take 5 (checked-location-s 3 1))
-
 
   (filter-location-s [3 1] 5)
   #_=> ([0 0] [1 3] [2 6] [3 9] [4 12])
@@ -70,22 +67,19 @@
   (location-char input-s [0 2])
   #_=> \#
 
-  (map tree? [\# \X \.])
-  #_=> (true true false)
-
-  (filter-location-s 3 1 (count input-s))
-  #_=> ([0 0] [1 3] [2 6] [3 9] [4 12] [5 15] [6 18] [7 21] [8 24] [9 27] [10 30])
-
   (mapv #(location-char input-s %) '([0 0] [1 3] [2 6] [3 9] [4 12] [5 15] [6 18] [7 21] [8 24] [9 27] [10 30]))
   #_=> [\. \O \X \O \X \X \O \X \X \X \X]
 
+  [(tree? \X) (tree? \#) (tree? \.) (tree? \A)]
+  #_=> [true true false false]
+
   (mapv tree? [\. \O \X \O \X \X \O \X \X \X \X])
-  #_=> [false false true false true true false true true true true]
+  #_=>  [false false true false true true false true true true true]
 
   (count (filter identity [false false true false true true false true true true true]))
   #_=> 7
 
-  (let [location-s      (filter-location-s 3 1 (count input-s))
+  (let [location-s      (filter-location-s [3 1] (count input-s))
         location-char-s (mapv #(location-char input-s %) location-s)]
     (->> location-char-s
          (mapv tree?)
@@ -98,7 +92,7 @@
   (do (def input-part1 (file->seq "resources/2020/day3/input.txt"))
       input-part1)
 
-  (let [location-s      (filter-location-s 3 1 (count input-part1))
+  (let [location-s      (filter-location-s [3 1] (count input-part1))
         location-char-s (mapv #(location-char input-part1 %) location-s)]
     (->> location-char-s
          (mapv tree?)
@@ -110,45 +104,47 @@
   (do (def input-s (file->seq "resources/2020/day3/input-sample.txt"))
       input-s)
 
-  (num-tree-solution input-s 3 1)
+  (count-tree input-s [3 1])
   #_=> 7
-  (num-tree-solution input-s 1 1)
+  (count-tree input-s [1 1])
   #_=> 2
-  (num-tree-solution input-s 5 1)
+  (count-tree input-s [5 1])
   #_=> 3
-  (num-tree-solution input-s 7 1)
+  (count-tree input-s [7 1])
   #_=> 4
-  (num-tree-solution input-s 1 2)
+  (count-tree input-s [1 2])
   #_=> 2
 
-  (let [val1 (num-tree-solution input-s 3 1)
-        val2 (num-tree-solution input-s 1 1)
-        val3 (num-tree-solution input-s 5 1)
-        val4 (num-tree-solution input-s 7 1)
-        val5 (num-tree-solution input-s 1 2)]
+  (let [val1 (count-tree input-s [3 1])
+        val2 (count-tree input-s [1 1])
+        val3 (count-tree input-s [5 1])
+        val4 (count-tree input-s [7 1])
+        val5 (count-tree input-s [1 2])]
     (* val1 val2 val3 val4 val5))
   #_=> 336
 
   ;; solution for part 2 real input
   (do (def input-part2 (file->seq "resources/2020/day3/input.txt"))
       input-part2)
-  (let [val1 (num-tree-solution input-part2 3 1)
-        val2 (num-tree-solution input-part2 1 1)
-        val3 (num-tree-solution input-part2 5 1)
-        val4 (num-tree-solution input-part2 7 1)
-        val5 (num-tree-solution input-part2 1 2)]
+  (let [val1 (count-tree input-part2 [3 1])
+        val2 (count-tree input-part2 [1 1])
+        val3 (count-tree input-part2 [5 1])
+        val4 (count-tree input-part2 [7 1])
+        val5 (count-tree input-part2 [1 2])]
     (* val1 val2 val3 val4 val5))
-  #_=> 704504196
+  #_=> 736527114
 
-  (num-tree-solution input-part2 3 1)
+  (count-tree input-part2 [3 1])
   #_=> 167
-  (num-tree-solution input-part2 1 1)
+  (count-tree input-part2 [1 1])
   #_=> 53
-  (num-tree-solution input-part2 5 1)
+  (count-tree input-part2 [5 1])
   #_=> 54
-  (num-tree-solution input-part2 7 1)
+  (count-tree input-part2 [7 1])
   #_=> 67
-  (num-tree-solution input-part2 1 2)
-  #_=> 22
+  (count-tree input-part2 [1 2])
+  #_=> 23
 
-)
+  (Math/ceil 2.3)
+  #_=> 3.0
+  )
